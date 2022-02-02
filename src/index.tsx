d4e227866b09d87a7be7202fc66c829746477b2f
aditReza <lete.note3@gmail.com>
2022-02-02 19:13:35 +0700

    add disable devtools in production

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import '../public/styles/main.css';
 import ReactDOM from "react-dom";
 import App from "./App";
+import { disableReactDevTools } from './config/disableReactDevTools';
 
+process.env.NODE_ENV === "production" && disableReactDevTools();
 ReactDOM.render(
   <React.StrictMode>
     <App />
